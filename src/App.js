60c95979b896401476fccf20f2ff6dee49d9b7f8
o6ris <loic.tsiry@gmail.com>
2022-10-21 18:17:50 +0200

    my first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+/** Import de la donnée */
+import './App.css';
+
+import Robots from './data/data';
+
+function App() {
+  console.log(Robots)
+  return (
+    <div className="App">
+      <h1>Robot Finder</h1>
+    </div>
+  );
+}
+
+export default App;
