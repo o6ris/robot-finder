3878d32f1891360bd5d1a0598d4a4a97fb249a7b
o6ris <loic.tsiry@gmail.com>
2022-10-22 13:09:42 +0200

    create different pages, home, robot finder and page 2 to simulate a naviguation.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import RobotFinder from './pages/RobotFinder';
 import Home from './pages/Home';
+import Page2 from './pages/Page2';
+
 
 function App() {
+  const [page, setPage] = useState('Home')
   return (
     <div>
-      <Home />
+      <div>
+        <nav>
+          <ul>
+            <li onClick={() => setPage('Home')}>Home</li>
+            <li onClick={() => setPage('RobotFinder')}>Robot Finder</li>
+            <li onClick={() => setPage('Page2')}>Page 2</li>
+          </ul>
+        </nav>
+      </div>
+      {page === 'RobotFinder'?<RobotFinder />:''}
+      {page === 'Home'?<Home />:''}
+      {page === 'Page2'?<Page2 />:''}
     </div>
   );
 }
