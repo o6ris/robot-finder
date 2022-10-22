cddeeddd0c5bd9a30ea57d3e7c13a5dd409fcf2e
o6ris <loic.tsiry@gmail.com>
2022-10-22 12:54:44 +0200

    I gonna create a navigation so first of all I create the page Home and add in it all we had in App.js and rectified all the link between components

--- src/App.js
+++ src/App.js
@@ -1,38 +1,11 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.css';
-import RoboCard from './components/RoboCard';
-import Robots from './data/data';
-import SearchRobot from './components/SearchRobot';
-import ShowMoreRobots from './components/ShowMoreRobots';
+import Home from './pages/Home';
+
 function App() {
-
-  const [searchRobot, setSearchRobot] = useState('')
-  const [numberOfRobots, setNumberOfRobots] = useState(0)
-
   return (
     <div>
-      <h1>Robot finder</h1>
-      {/* search bar */}
-      <SearchRobot setSearchRobot={setSearchRobot}/>
-
-      {/* robots display */}
-      <div className="robots-display">
-
-        {/* filter Robots per name, display only 4 robots with slice and map function. */}
-        {Robots
-        .filter(robot => robot.name.toLowerCase().includes(searchRobot.toLowerCase()))
-        .slice(0,numberOfRobots+4)
-        .map((robot,index) =>{
-          return(
-            <div>
-              <RoboCard index={index} robot={robot}  />
-            </div>
-          )
-        })}
-      </div>
-
-      {/* show more button */}  
-      <ShowMoreRobots numberOfRobots={numberOfRobots} setNumberOfRobots={setNumberOfRobots} robots={Robots}/>
+      <Home />
     </div>
   );
 }
