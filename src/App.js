7b22ea04019681a34edc49c48d6d0b53401700cd
o6ris <loic.tsiry@gmail.com>
2022-10-22 11:18:58 +0200

    add the state searchRobot, filter robots including the value searchRobot and add the eventlistener onChange on the input with setSearchRobot

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import RoboCard from './components/RoboCard';
 import Robots from './data/data';
 
 function App() {
-  
+
+  const [searchRobot, setSearchRobot] = useState('')
+
+
   return (
     <div>
       <h1>Robot finder</h1>
       <div className='text-center'>
-        <input className='robot-search-bar' type="text" placeholder='Search a robot' />
+        <input onChange={(e) => setSearchRobot(e.target.value)} className='robot-search-bar' type="text" placeholder='Search a robot' />
       </div>
       <div className="robots-display">
         
-        {Robots.map((robot,index) =>{
+        {Robots
+        .filter(robot => robot.name.includes(searchRobot))
+        .map((robot,index) =>{
           return(
             <div>
-              
               <RoboCard index={index} robot={robot}  />
             </div>
           )
